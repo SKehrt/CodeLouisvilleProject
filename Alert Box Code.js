59189192d1b00ceab4d9f02277789a1b4055dd4d
SKehrt <35316646+SKehrt@users.noreply.github.com>
2018-03-29 10:47:19 -0400

    added JS alert box code

---
+++ Alert Box Code.js
@@ -0,0 +1,15 @@
+
+<form name= About Stephanie>
+<input type=button value="About Stephanie"
+onClick="alert('20 Years of Portrait Experience!')">
+</form>
+
+<form name= Price Information>
+<input type=button value="Prices"
+onClick="alert('Please Call for Package Prices & Details')">
+</form>
+
+<form name= Contact Information>
+<input type=button value="Contact Information"
+onClick="alert('Please Call or Email for to Make an Appointment. Thank you!')">
+</form>
